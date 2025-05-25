16c55f36f6d408d671810c545ce85a35a5894bbc
marjorieisabel <amourakayleigh@gmail.com>
2025-05-26 02:45:01 +0700

    Create utils.py

---
+++ utils.py
@@ -0,0 +1,80 @@
+from datetime import datetime, timedelta
+
+banned_words = ['anjing', 'bangsat', 'kontol', 'tolol']
+
+pending_users = set()
+user_fess_count = {}  # {user_id: [(date, count), ...]}
+user_last_messages = {}  # {user_id: [(message_id, datetime), ...]}
+leaderboard = {}
+anon_chats = {}
+user_profiles = {}
+
+DEFAULT_LANG = "id"
+
+TEXTS = {
+    "id": {
+        "menu": "Hai! Pilih menu di bawah ini:",
+        # ... (seluruh dict seperti di atas)
+    },
+    "en": {
+        "menu": "Hi! Please choose a menu below:",
+        # ... (seluruh dict seperti di atas)
+    }
+}
+
+def get_text(user_id, key):
+    lang = user_profiles.get(user_id, {}).get("lang", DEFAULT_LANG)
+    return TEXTS.get(lang, TEXTS[DEFAULT_LANG]).get(key, "")
+
+def contains_bad_words(text):
+    return any(word in text.lower() for word in banned_words)
+
+def can_send(user_id):
+    today = datetime.now().date()
+    if user_id not in user_fess_count:
+        user_fess_count[user_id] = [(today, 1)]
+        return True
+    date_list = user_fess_count[user_id]
+    if date_list[-1][0] != today:
+        user_fess_count[user_id] = [(today, 1)]
+        return True
+    elif date_list[-1][1] < 15:
+        user_fess_count[user_id][-1] = (today, date_list[-1][1] + 1)
+        return True
+    else:
+        return False
+
+def update_leaderboard(user_id):
+    leaderboard[user_id] = leaderboard.get(user_id, 0) + 1
+
+def add_fess_message(user_id, message_id):
+    now = datetime.now()
+    if user_id not in user_last_messages:
+        user_last_messages[user_id] = []
+    user_last_messages[user_id].append((message_id, now))
+    user_last_messages[user_id] = [(mid, dt) for (mid, dt) in user_last_messages[user_id] if now - dt <= timedelta(minutes=60)]
+
+def find_anon_partner(user_id):
+    waiting = [uid for uid in anon_chats if anon_chats[uid] is None and uid != user_id]
+    if waiting:
+        partner = waiting[0]
+        anon_chats[user_id] = partner
+        anon_chats[partner] = user_id
+        return partner
+    else:
+        anon_chats[user_id] = None
+        return None
+
+def end_anon_chat(user_id):
+    partner = anon_chats.get(user_id)
+    if partner is not None:
+        anon_chats.pop(partner, None)
+    anon_chats.pop(user_id, None)
+
+def mask_username(username):
+    if not username:
+        return "anonymous"
+    if len(username) <= 2:
+        return username[0] + "*"
+    else:
+        return username[0] + "*"*(len(username)-2) + username[-1]
